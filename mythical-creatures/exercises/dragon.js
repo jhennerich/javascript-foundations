34c75b7a2ccaf709ba1e5b7647d1cef909709222
John Hennerich <john.hennerich@gmail.com>
2022-11-02 21:11:35 -0600

    Dragon tests pass

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+    constructor(name, rider ) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.meals = 0;
+    }
 
+    greet() {
+        const message = "Hi, " + this.rider + "!";
+        return message;
+    }
+
+    eat() {
+        this.meals += 1;
+        if (this.meals === 3) {
+            this.hungry = false;
+
+        }
+    }
+}
+module.exports = Dragon;
